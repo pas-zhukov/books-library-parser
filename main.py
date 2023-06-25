29286cad5f1ca79276ae1c3b9cc8c9416ab12376
Pavel <pas-zhukov@yandex.ru>
2023-06-25 23:42:19 +0300

    developing parser

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import requests
 from pathlib import Path
 
+PATH_TO_BOOKS = "books"
+
+
 def download_txt(url: str, path: str):
     response = requests.get(url)
     response.raise_for_status()
+    check_for_redirect(response)
     with open(path, "xb+") as file:
         file.write(response.content)
 
 
-Path("books").mkdir(parents=True, exist_ok=True)
-for i in range(1,11):
-    download_txt(f'https://tululu.org/txt.php?id={i}', f'books/book_id{i}.txt')
+def check_for_redirect(response: requests.Response):
+    if response.history:
+        raise requests.HTTPError
+    else:
+        return True
+
+
+def main():
+    Path("books").mkdir(parents=True, exist_ok=True)
+    for idx in range(1, 11):
+        try:
+            download_txt(f'https://tululu.org/txt.php?id={idx}', f'books/book_id{idx}.txt')
+        except requests.HTTPError:
+            continue
+
+
+if __name__ == "__main__":
+    main()
