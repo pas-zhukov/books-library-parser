4834fa0bf262419a3b4925297d9ad033fb021d0a
Pavel <pas-zhukov@yandex.ru>
2023-06-26 21:57:54 +0300

    formatting code. add some docstrings

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import textwrap as tw
 from argparse import ArgumentParser
 from urllib.parse import urljoin
 from pathlib import Path
@@ -30,30 +31,51 @@ def main():
         default=10,
         type=int
     )
+    arg_parser.add_argument('--list',
+                            action='store_true',
+                            help='Use this flag if you want the list of books to be printed after download.')
     args = arg_parser.parse_args()
     books_ids = args.start_id, args.end_id
 
-    books_folder = os.getenv("BOOKS_PATH")
-    images_folder = os.getenv("IMAGES_PATH")
+    books_folder = os.getenv("BOOKS_PATH", 'downloaded_books')
+    images_folder = os.getenv("IMAGES_PATH", 'downloaded_images')
 
+    downloaded_books = []
     for book_id in tqdm(range(*books_ids)):
         try:
-            response = requests.get(f"{SITE_URL}/b{book_id}")
+            response = requests.get(f"{SITE_URL}/b{book_id}/")
             response.raise_for_status()
             raise_if_redirect(response)
             page_html = response.text
 
             book = ParsedBook(page_html=page_html)
+            downloaded_books.append(book)
 
-            download_txt(f"{SITE_URL}/txt.php", f"{book_id}. {book.title}", books_folder, params={'id': book_id})
-            download_image(book.image['url'], book.image['filename'], images_folder)
+            download_txt(f"{SITE_URL}/txt.php",
+                         f"{book_id}. {book.title}.txt",
+                         books_folder,
+                         params={'id': book_id})
+            download_image(book.image['url'],
+                           book.image['filename'],
+                           images_folder)
         except RedirectDetectedError:
             continue
 
-    print("Done!")
+    print("Download complete!")
+    if args.list:
+        print("Books list: \n")
+        for book in downloaded_books:
+            print(book)
 
 
 class ParsedBook:
+    """
+
+    The ParsedBook class is responsible for parsing the HTML page of a book
+    from the Tululu website and extracting relevant information
+    such as the book title, author, genre, image URL, and comments.
+    It also provides a string representation of the book object.
+    """
     def __init__(self, page_html):
         self.page_html = page_html
 
@@ -81,9 +103,7 @@ class ParsedBook:
     @property
     def comments(self):
         _comments = BeautifulSoup(self.page_html, 'lxml').find('div', {'id': 'content'}).find_all('div', {'class': 'texts'})
-        comments_texts = []
-        for comment in _comments:
-            comments_texts.append(comment.find('span').get_text())
+        comments_texts = [comment.find('span').get_text() for comment in _comments]
         return comments_texts
 
     @property
@@ -91,22 +111,44 @@ class ParsedBook:
         genre = BeautifulSoup(self.page_html, 'lxml').find('span', class_="d_book").find('a').get_text()
         return genre
 
+    def __str__(self):
+        text = f"""
+            Название: {self.title}
+            Автор: {self.author}
+            Жанр: {self.genre}"""
+        text = tw.dedent(text)
+        return text
 
-def download_txt(url: str, filename: str, folder: str = 'downloaded_texts', params: dict = None) -> str:
+
+def download_txt(url: str,
+                 filename: str,
+                 folder: str = 'downloaded_texts',
+                 params: dict = None) -> str:
+    """
+
+    This function downloads a txt file from a given URL
+    and save it to a specified path on the local machine.
+
+    :param url: the URL of the text file to be downloaded
+    :param filename: name of the text file on your computer
+    :param folder: path to a folder where the text file will be saved
+    :param params: optional parameters to be passed in the request
+    :return: the path of the saved text file
+    """
     Path(folder).mkdir(parents=True, exist_ok=True)
     path = os.path.join(folder, filename)
     response = requests.get(url, params=params)
     response.raise_for_status()
     raise_if_redirect(response)
 
-    with open(path, "xb+") as file:
+    with open(path, "wb+") as file:
         file.write(response.content)
     return path
 
 
 def download_image(image_url: str,
                    filename: str,
-                   folder: str = 'downloaded_images'):
+                   folder: str = 'downloaded_images') -> str:
     """
 
     This function downloads an image from a given URL
@@ -115,7 +157,7 @@ def download_image(image_url: str,
     :param image_url: the URL of the image to be downloaded
     :param filename: name of the image file on your computer
     :param folder: path to a folder where image will be saved
-    :return: None
+    :return: the path of the saved image file
     """
     Path(folder).mkdir(parents=True, exist_ok=True)
     path = os.path.join(folder, filename)
