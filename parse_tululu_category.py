243f64ad53bd6387b4ceb85ddaa3d9ed7c79d7c2
Pavel <pas-zhukov@yandex.ru>
2023-07-03 21:51:51 +0300

    format code

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -10,7 +10,8 @@ from dotenv import load_dotenv
 import requests
 from tqdm import tqdm, trange
 
-from main import download_image, download_txt, parse_book_page, raise_if_redirect, SITE_URL, RedirectDetectedError
+from main import download_image, download_txt, parse_book_page
+from main import SITE_URL, RedirectDetectedError, raise_if_redirect
 
 
 def main():
@@ -21,11 +22,14 @@ def main():
         try:
             end_page = get_pages_count(f"{SITE_URL}/l{args.category_id}/1/")
         except RedirectDetectedError:
-            tqdm.write(f"Unable to download pages count. Probably wrong category ID.")
+            tqdm.write("Unable to download pages count."
+                       " Probably wrong category ID.")
         except requests.HTTPError:
-            tqdm.write(f"Unable to download pages count. Wrong URL. Check the category ID.")
+            tqdm.write("Unable to download pages count. "
+                       "Wrong URL. Check the category ID.")
         except requests.ConnectionError:
-            tqdm.write(f"Connection Error! Please check your internet connection.")
+            tqdm.write("Connection Error! Please check your"
+                       " internet connection.")
     else:
         end_page = args.end_page
     if args.start_page > end_page:
@@ -41,10 +45,12 @@ def main():
     connection_timeout = os.getenv("CONNECTION_TIMEOUT", 120)
 
     try:
-        book_links = parse_category(args.category_id, args.start_page, end_page)
+        book_links = parse_category(args.category_id,
+                                    args.start_page,
+                                    end_page)
         downloaded_books = []
-        for book_link in tqdm(book_links, desc='Downloading books'):
-            book_id = re.search(r'\d+', urlsplit(book_link).path).group()
+        for book_link in tqdm(book_links, desc="Downloading books"):
+            book_id = re.search(r"\d+", urlsplit(book_link).path).group()
             try:
                 response = requests.get(book_link)
                 response.raise_for_status()
@@ -53,22 +59,28 @@ def main():
 
                 book = parse_book_page(page_html=page_html, page_url=book_link)
                 if not args.skip_txt:
-                    book['book_path'] = download_txt(f"{SITE_URL}/txt.php",
-                                                     f"{book['title']}.txt",
-                                                     books_folder,
-                                                     params={'id': book_id})
+                    book["book_path"] = download_txt(
+                        f"{SITE_URL}/txt.php",
+                        f"{book['title']}.txt",
+                        books_folder,
+                        params={"id": book_id}
+                    )
                 if not args.skip_imgs:
-                    book['img_src'] = download_image(book.pop("image_url"),
-                                                     book.pop("image_filename"),
-                                                     images_folder)
+                    book["img_src"] = download_image(
+                        book.pop("image_url"),
+                        book.pop("image_filename"),
+                        images_folder
+                    )
                 downloaded_books.append(book)
             except RedirectDetectedError:
-                # Метод write() используется чтобы прогресс-бар не ломался из-за вывода через print()
+                # Метод write() используется чтобы прогресс-бар
+                # не ломался из-за вывода через print()
                 tqdm.write(f"Book with ID={book_id} wasn't "
                            "downloaded because there is no TXT for this book.")
             except requests.HTTPError:
                 tqdm.write(f"Book with ID={book_id} wasn't "
-                           "downloaded because URL used for downloading this book "
+                           "downloaded because URL used for "
+                           "downloading this book "
                            "is wrong or incorrect.")
             except requests.ConnectionError:
                 tqdm.write(f"Book with ID={book_id} wasn't "
@@ -77,28 +89,37 @@ def main():
                            "internet connection to restore.")
                 time.sleep(connection_timeout)
         downloaded_books = json.dumps(downloaded_books, ensure_ascii=False)
-        with open(os.path.join(master_folder, "books_metadata.json"), 'w+', encoding='utf-8') as file:
+        with open(os.path.join(master_folder, "books_metadata.json"),
+                  "w+",
+                  encoding="utf-8") as file:
             file.write(downloaded_books)
     except RedirectDetectedError:
-        tqdm.write(f"Unable to download links. Probably out of pages count."
-                   f" Check the amount of pages per chosen category.")
+        tqdm.write("Unable to download links. Probably out of pages count."
+                   " Check the amount of pages per chosen category.")
     except requests.HTTPError:
-        tqdm.write(f"Unable to download links. Wrong URL. Check the category ID.")
+        tqdm.write("Unable to download links. "
+                   "Wrong URL. Check the category ID.")
     except requests.ConnectionError:
-        tqdm.write(f"Connection Error! Please check your internet connection.")
+        tqdm.write("Connection Error! "
+                   "Please check your internet connection.")
 
 
-def parse_category(category_id: int, start_page: int = 1, end_page: int = None) -> list:
+def parse_category(category_id: int,
+                   start_page: int = 1,
+                   end_page: int = None) -> list:
     """
     Parses the book links from a given category on the Tululu website.
 
-    :param category_id: ID of the category to download.
-    :param start_page:  page number from which to begin downloading books (in category).
+    :param category_id: ID of the category to download
+    :param start_page: page number from which to begin downloading
     :param end_page: end page where to stop
     :return: A list of book links
     """
     book_links = []
-    for page_index in trange(start_page, end_page + 1, desc=f'Downloading books links from category with ID={category_id}'):
+    for page_index in trange(start_page,
+                             end_page + 1,
+                             desc=f"Downloading books links from "
+                                  f"category with ID={category_id}"):
         category_url = f"{SITE_URL}/l{category_id}/{page_index}/"
 
         response = requests.get(category_url)
@@ -106,12 +127,12 @@ def parse_category(category_id: int, start_page: int = 1, end_page: int = None) 
         raise_if_redirect(response)
         page_html = response.text
 
-        soup = BeautifulSoup(page_html, 'lxml')
-        selector = 'div[id="content"] table.d_book'
+        soup = BeautifulSoup(page_html, "lxml")
+        selector = "div[id='content'] table.d_book"
         books = soup.select(selector)
         for book in books:
-            url_selector = 'a'
-            book_url = book.select_one(url_selector).get('href')
+            url_selector = "a"
+            book_url = book.select_one(url_selector).get("href")
             book_full_url = urljoin(category_url, book_url)
             book_links.append(book_full_url)
     return book_links
@@ -119,7 +140,8 @@ def parse_category(category_id: int, start_page: int = 1, end_page: int = None) 
 
 def get_pages_count(category_page_url: str) -> int:
     """
-    Extracts the total number of pages for a given category on the Tululu website.
+    Extracts the total number of pages
+    for a given category on the Tululu website.
 
     :param category_page_url: URL of the category page to be parsed
     :return: total number of pages for the given category
@@ -128,8 +150,8 @@ def get_pages_count(category_page_url: str) -> int:
     response.raise_for_status()
     raise_if_redirect(response)
     category_page = response.text
-    soup = BeautifulSoup(category_page, 'lxml')
-    selector = 'a.npage:nth-child(7)'
+    soup = BeautifulSoup(category_page, "lxml")
+    selector = "a.npage:nth-child(7)"
     _pages_count = soup.select_one(selector)
     pages_count = int(_pages_count.get_text())
     return pages_count
@@ -137,17 +159,20 @@ def get_pages_count(category_page_url: str) -> int:
 
 def parse_args():
     """
-    Parse the command-line arguments provided by the user and returning them as an object.
+    Parse the command-line arguments provided
+    by the user and returning them as an object.
 
     :return: object containing the parsed arguments as attributes
     """
     arg_parser = ArgumentParser(
-        description="This program allows to download some books from tululu specified by genre."
+        description="This program allows to download "
+                    "some books from tululu specified by genre."
     )
     arg_parser.add_argument(
         "-s",
         "--start_page",
-        help="Page number from which to begin downloading books (in category).",
+        help="Page number from which to "
+             "begin downloading books (in category).",
         default=1,
         type=int
     )
@@ -184,5 +209,5 @@ def parse_args():
     return args
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
